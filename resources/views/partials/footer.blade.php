034e6d6c8cc6de05d613a5f22a942459b11be4f3
Taiwo Emmanuel <laffbaze@gmail.com>
2020-09-18 13:25:03 +0100

    responsive slide and clip path

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -19,11 +19,10 @@
            </div>
            <div class="col-md-4">
             <h4>Connect</h4>
-               <h4>Important link</h4>
-             <div>Phone number</div>
-             <div>Email Address</div>
+             <div><i class="fa fa-phone" aria-hidden="true"></i><a href="tel:070373836363">+2340938373563<a> </div>
+             <div><i class="fa fa-evelope" aria-hidden="true"></i> Email Address</div>
              <div>
-                 Socail links             </div>
+                 Socail links </div>
            </div>
         </div>
     </div>
@@ -33,7 +32,13 @@
             <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
         </div>
     </div>
+    <svg width="0" height="0">
+        <defs>
+          <clipPath id="myClip">
+            <path d="M680.452 1H-2.5V242.5V385.5H718H1439V42H738.048C735.753 42 733.527 41.2103 731.745 39.7635L686.755 3.23651C684.973 1.78969 682.747 1 680.452 1Z" stroke="black"/>
+          </clipPath>
+        </defs>
+      </svg>
 </footer>
-
 
 </div>
--- resources/views/partials/slider.blade.php
+++ resources/views/partials/slider.blade.php
@@ -6,28 +6,26 @@
             <li data-target="#carouselId" data-slide-to="2"></li>
         </ol>
         <div class="carousel-inner" role="listbox" style="width:100%">
-            <div class="carousel-item active " >
-                <img src="{{ asset('images/slider/1.jpg')}}" width="100%" alt="First slide">
-                <div class="carousel-caption animated bounceInLeft" style="animation-delay:0.5s">
-                    <h3 class="animated bounceInLeft"  style="animation-delay:0.8s">Leadership</h3>
+            <div class="carousel-item active min-vh-100" style="background:url({{ asset('images/slider/1.jpg') }}) no-repeat;background-size:cover">
+                <!-- <img src="{{ asset('images/slider/1.jpg')}}" width="100%" alt="First slide"> -->
+                <div class="carousel-caption animated bounceInLeft" style="animation-delay:0.5s;">
+                    <h3 class="animated bounceInLeft" style="animation-delay:0.8s">Leadership</h3>
                     <p>He who walk must learn how to fly, you cannot <span>fly</span> into fly</p>
                     <button class="btn btn-primary animated zoomIn" style="animation-delay:1s">Learn more</button>
                 </div>
             </div>
-            <div class="carousel-item">
-                <img src="{{ asset('images/slider/2.jpg')}}" width="100%" alt="Second slide">
+            <div class="carousel-item min-vh-100" style="background:url({{ asset('images/slider/2.jpg') }}) no-repeat;background-size:cover">
                 <div class="carousel-caption animated bounceInRight" style="animation-delay:0.5s">
                     <h3 class="animated bounceInLeft" style="animation-delay:0.8s">Let's go global</h3>
                     <p>Description</p>
                     <button class="btn btn-primary animated zoomInUp" style="animation-delay:1s">Learn more</button>
                 </div>
             </div>
-            <div class="carousel-item">
-                <img src="{{ asset('images/slider/3.jpg')}}" width="100%" alt="Third slide">
+            <div class="carousel-item min-vh-100"  style="background:url({{ asset('images/slider/3.jpg')  }}) no-repeat;background-size:cover">
                 <div class="carousel-caption animated bounceInLeft" style="animation-delay:0.5s">
                     <h3 class="animated bounceInRight" style="animation-delay:0.8s">Arise Room</h3>
-                    <p>A thousand <span>mile</span> begings  </p>
-                    <button class="btn btn-primary animated bounceInLeft" style="animation-delay:1s" >Learn more</button>
+                    <p>A thousand <span>mile</span> begings </p>
+                    <button class="btn btn-primary animated bounceInLeft" style="animation-delay:1s">Learn more</button>
                 </div>
             </div>
         </div>
@@ -40,6 +38,19 @@
             <span class="sr-only">Next</span>
         </a>
     </div>
+
+    <svg width="0" height="0" viewBox="0 0 600 400">
+        <defs>
+            <clipPath id="myClipSlider">
+                <path class=" d-none d-sm-block d-md-block"
+                    d="M680.531 577H-1V151.784V-100H719.75H1441V504.81H741.463C738.199 504.81 735.141 506.403 733.27 509.077L688.724 572.733C686.853 575.407 683.794 577 680.531 577Z"
+                    stroke="black" />
+                        <path class="d-sm-none" d="M478 1H0V571.5H189.728C192.457 571.5 195.067 572.615 196.953 574.586L248.023 627.957C248.967 628.943 250.272 629.5 251.636 629.5H478V322V1Z" stroke="black"/>
+                        
+                                  
+
+            </clipPath>
+        </defs>
+    </svg>
+
 </div>
-
-
